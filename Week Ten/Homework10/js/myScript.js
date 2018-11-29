06e6b2ddda2081a2650125e35bbe1023cdd0871d
williamburbatt <williamburbatt@yahoo.com>
2018-11-28 19:46:14 -0600

    finished Homework 10. Fibonacci Seq with Javascript.

--- Week Ten/Homework10/js/myScript.js
+++ Week Ten/Homework10/js/myScript.js
@@ -1,12 +1,23 @@
-function fib(input){
-    if(input <= 1)
+var button = document.getElementById("submitButton");
+button.addEventListener("click", fibHelper, true);
+
+
+function fib(input) {
+    if (input <= 1)
         return 1;
-    return fib(input - 1) + fib(input-2);
+    return fib(input - 1) + fib(input - 2);
 }
 
-function fibHelper(){
-var input =  document.getElementById('input').value;   
-var diff = document.getElementById('result'); // Get element
-diff.textContent = fib(input);
-}
+function fibHelper() {
+    var input = document.getElementById('input').value;
+    var diff = document.getElementById('result'); // Get element
+    var finalString = "";
+    var x = input;
 
+    while (x > 0) {
+        var curr = fib(x);
+        finalString = finalString + curr + " ";
+        x--;
+    }
+    diff.textContent = finalString;
+}
