489c9114c0f5304b3f7d5ff4f57d93ad5f48fe4d
williamburbatt <williamburbatt@yahoo.com>
2018-12-03 18:15:43 -0600

    fixed some tabbing need to work on layout of page.

--- Week Eleven/Homework11/js/myScript.js
+++ Week Eleven/Homework11/js/myScript.js
@@ -1,23 +1,21 @@
-
 $(function () {
     $("#submitButton").click(function () {
-    var text = $('#input').val();
-    if (text != "") {
-        $("ul.inProgress").append('<li>' + text + '</l1>');
-    }
-    $('#input').val("");
-          $('li').on('click', function () {
-        var text = $(this).text();
-        $(this).animate({
-            opacity: 0.0,
-            paddingLeft: '+=80'
-        }, 500, function () {
-        $("ul.finished").append('<li>' + text + '</l1>');
+        var text = $('#input').val();
+        if (text != "") {
+            $("ul.inProgress").append('<li>' + text + '</l1>');
+        }
+        $('#input').val("");
+        $('li').on('click', function () {
+            var text = $(this).text();
+            $(this).animate({
+                opacity: 0.0,
+                paddingLeft: '+=80'
+            }, 500, function () {
+                $("ul.finished").append('<li>' + text + '</l1>');
 
-            $(this).remove();
+                $(this).remove();
+            });
         });
     });
-});
-    
-});
 
+});
