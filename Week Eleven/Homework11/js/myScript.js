62ecfda3fee6efa3bf4ecbd43e0c98ea4e427873
williamburbatt <williamburbatt@yahoo.com>
2018-12-03 18:04:38 -0600

    Final Project

---
+++ Week Eleven/Homework11/js/myScript.js
@@ -0,0 +1,24 @@
+$(function () {
+    $("#submitButton").click(function () {
+        var text = $('#input').val();
+        if (text != "") {
+            $("ul.inProgress").append('<li>' + text + '</l1>');
+        }
+        $('#input').val("");
+    });
+});
+
+
+$(function () {
+    $('li').on('click', function () {
+        var text = $(this).text();
+        $(this).animate({
+            opacity: 0.0,
+            paddingLeft: '+=80'
+        }, 500, function () {
+            $("ul.finished").append('<li>' + text + '</l1>');
+
+            $(this).remove();
+        });
+    });
+});
