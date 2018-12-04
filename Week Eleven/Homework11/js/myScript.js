32388febaaa2745dcb9d85edafa4aa9358d5b53c
williamburbatt <williamburbatt@yahoo.com>
2018-12-03 18:32:14 -0600

    fixed so only top ul is clickable.

--- Week Eleven/Homework11/js/myScript.js
+++ Week Eleven/Homework11/js/myScript.js
@@ -2,16 +2,17 @@ $(function () {
     $("#submitButton").click(function () {
         var text = $('#input').val();
         if (text != "") {
-            $("ul.inProgress").append('<li>' + text + '</l1>');
+            $("ul.inProgress").append('<li class =\'added\'>' + text + '</l1>');
         }
         $('#input').val("");
-        $('li').on('click', function () {
+        $('li.added').on('click', function () {
             var text = $(this).text();
             $(this).animate({
                 opacity: 0.0,
                 paddingLeft: '+=80'
             }, 500, function () {
                 $("ul.finished").append('<li>' + text + '</l1>');
+                
 
                 $(this).remove();
             });
