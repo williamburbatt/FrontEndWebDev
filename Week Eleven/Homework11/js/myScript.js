21a07b3112a2dbf4bc89b74d4b12dd8fb92fa03b
williamburbatt <williamburbatt@yahoo.com>
2018-12-04 10:40:48 -0600

    styling

--- Week Eleven/Homework11/js/myScript.js
+++ Week Eleven/Homework11/js/myScript.js
@@ -1,10 +1,24 @@
+/**
+Im still not sure if these should be two functions or if wrapping them in the same function is how its supposed to work.
+**/
+
 $(function () {
+    /**
+    Learned about keyup events. keycode 13 strict is enter pressed
+
+    https://stackoverflow.com/questions/155188/trigger-a-button-click-with-javascript-on-the-enter-key-in-a-text-box
+
+    **/
     
-$("#input").keyup(function(event) {
-    if (event.keyCode === 13) {
-        $("#submitButton").click();
-    }
-});
+    $("#input").keyup(function (event) {
+        if (event.keyCode === 13) {
+            $("#submitButton").click();
+        }
+    });
+    
+    /**
+    written with help from slides mostly my code though.
+    **/
     
     $("#submitButton").click(function () {
         var text = $('#input').val();
@@ -19,7 +33,7 @@ $("#input").keyup(function(event) {
                 paddingLeft: '+=80'
             }, 500, function () {
                 $("ul.finished").append('<li>' + text + '</l1>');
-                
+
 
                 $(this).remove();
             });
